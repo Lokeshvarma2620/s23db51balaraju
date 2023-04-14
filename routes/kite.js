330716a174a1eaa67dc222ca801eecc8f1010889
Lokeshvarma2620 <Lokeshvarma1014@gmail.com>
2023-04-14 12:53:25 -0500

    updated

--- routes/kite.js
+++ routes/kite.js
@@ -1,17 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('kite', { title: 'Search results for kite class' });
-});
-
-var express = require('express');
 const kite_controlers= require('../controllers/kite');
-var router = express.Router();
 /* GET kite */
 router.get('/', kite_controlers.kite_view_all_Page );
-module.exports = router;
-
 
 module.exports = router;
